2e32620055c06ec927c27386b4397ad3929058db
Andro Bakashvili <bakashviliandro@gmail.com>
2024-04-29 17:44:16 +0400

    Update EmptyInputException.cs

--- QuizApp/Exceptions/EmptyInputException.cs
+++ QuizApp/Exceptions/EmptyInputException.cs
@@ -6,5 +6,5 @@ public class EmptyInputException : Exception
 
     public EmptyInputException(string message) : base(message) { }
 
-    public EmptyInputException (string message, Exception inner) : base (message, inner) { }
+    public EmptyInputException(string message, Exception inner) : base(message, inner) { }
 }
